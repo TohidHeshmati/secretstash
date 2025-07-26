f50510a86a6ff0d31691e311ed50316e86e7c2b7
Tohid Heshmati <tohidfla@gmail.com>
2025-07-26 16:53:49 +0200

    change table name

--- src/main/kotlin/com/tohid/secretstash/domain/Note.kt
+++ src/main/kotlin/com/tohid/secretstash/domain/Note.kt
@@ -11,7 +11,7 @@ import jakarta.persistence.Table
 import java.time.Instant
 
 @Entity
-@Table(name = "notes")
+@Table(name = "note")
 data class Note(
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long = 0,
